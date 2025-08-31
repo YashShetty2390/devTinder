49b9e8099c08554f509e97558406b0bebb588d15
Yashashwini Shetty <yashashwinisshetty@gmail.com>
2025-08-31 12:43:05 +0530

    error handling changes

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ const { adminAuth } = require("./middlewares/adminAuth");
 const app = express();
 
 app.use("/admin", adminAuth);
-app.use("/admin/getAdminData", (req, res) => {
+app.use("/admin/getAdminData", (req, res, next) => {
+
     res.send("Fetched Admin Data")
+
 })
 app.use("/admin/deleteAdminData", (req, res) => {
     res.send("Deleted Admin Data");
 });
+app.use("/user", (req, res) => {
+    console.log("Inside wildcard route");
+    throw new error("trtrtrt");
+    //res.send("Hello from server ");
 
-app.use("/", (req, res) => {
-    res.send("Hello from server ");
 });
+app.use((err, req, res, next) => {
+    console.log("Inside wildcard route");
+    if (err) {
+        res.status(500).send("Something went wrong");
+    }
+});
+
+
 app.listen(4000, () => {
     console.log("Server is up")
 });
