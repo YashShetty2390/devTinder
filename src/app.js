416b939d3b3e3b2be7e97e60178f8ed39b888e96
Yashashwini Shetty <yashashwinisshetty@gmail.com>
2025-09-01 19:42:49 +0530

    login and signup changes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const { adminAuth } = require("./middlewares/adminAuth");
 const app = express();
 const { connectDB } = require("./config/database");
 const User = require("./models/User");
-
+const bcrypt = require("bcrypt");
 /*app.use("/admin", adminAuth);
 app.use("/admin/getAdminData", (req, res, next) => {
 
@@ -27,22 +27,48 @@ app.use("/", (err, req, res, next) => {
 });*/
 app.use(express.json());
 app.post("/signup", async (req, res) => {
-    console.log(req.body);
     const userObj = new User(req.body);
     try {
+        const salt = await bcrypt.genSalt(10);
+        userObj.password = await bcrypt.hash(userObj.password, salt);
         await userObj.save();
         res.send("User added successfully");
     } catch (err) {
-        res.status(500).send("Error occured while saving data");
+        res.status(500).send("Error occured while saving data", +err);
     }
-})
+});
 
 
+app.post("/login", async (req, res) => {
+    const { emailId, password } = req.body;
+    try {
+        const user = await User.find({ emailId });
+        if (user.length === 0) {
+            return res.status(400).send("Invalid email or password");
+        }
+        const isMatch = await bcrypt.compare(password, user[0].password);
+        if (!isMatch) {
+            return res.status(400).send("Invalid email or password");
+        }
+        res.send("Login successful");
+    } catch (err) {
+        res.status(500).send("Error occured while logging in");
+    }
+});
+
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).send("Error occured while fetching data");
+    }
+});
 connectDB().then(() => {
     console.log("Database connection established");
     app.listen(4000, () => {
         console.log("Server is up")
     });
 }).catch((err) => {
-    console.log("Database connection failed");
+    console.log("Database connection failed", err.message);
 });
--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true
     },
     lastName: {
         type: String
@@ -10,14 +12,37 @@ const userSchema = new mongoose.Schema({
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid Email Id");
+            }
+        }
     },
     password: {
-        type: String
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Password is not strong enough");
+            }
+        }
     },
     age: {
         type: Number
     },
-});
+    photoUrl: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+    },
+    bio: {
+        type: String,
+        maxlength: 250,
+        default: "Hello! I am using DevTinder."
+    }
+}, { timestamps: true });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
